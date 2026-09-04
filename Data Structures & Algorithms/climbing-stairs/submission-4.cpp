fcfd1e21efeba64a16e46c3e9f02df4491e35921
Harsh Tandon <135816287+harshtandon6062@users.noreply.github.com>
2026-09-04 14:19:04 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int first_prv = 1, second_prv = 1;
+        for(int i = 2; i <= n; i++){
+            swap(first_prv, second_prv);
+            first_prv += second_prv;
+        }
+        return first_prv;
+    }
+};
+// 1,1,2,3
+// 5
+// 221
+// 212
+// 122
+// 2111
+// 1211
+// 1121
+// 1112
+// 11111
